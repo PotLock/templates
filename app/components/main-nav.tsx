18f68e4fafee68fc695b9ef0a856d36df7fd50eb
codingshot <45281667+codingshot@users.noreply.github.com>
2024-10-08 23:36:43 -0700

    added images for tempaltes examples

--- app/components/main-nav.tsx
+++ app/components/main-nav.tsx
@@ -12,19 +12,21 @@ interface MainNavProps {
 
 export function MainNav({ items }: MainNavProps) {
   return (
-    <div className="flex gap-6 md:gap-10">
+    <div className="flex items-center justify-center gap-6 md:gap-10">
       <Link href="/" className="flex items-center space-x-2">
         {/* <Icons.logo className="h-6 w-6" /> */}
-        <span className="inline-block font-bold">🫕  {siteConfig.name}</span>
+        <span className="inline-block font-bold">🫕 {siteConfig.name}</span>
       </Link>
       {items?.length ? (
-        <nav className="flex gap-6">
+        <nav className="flex justify-center align-center items-center gap-6">
           {items?.map(
             (item, index) =>
               item.href && (
                 <Link
                   key={index}
                   href={item.href}
+                  target="_blank"
+                  rel="noopener noreferrer"
                   className={cn(
                     "flex items-center text-sm font-medium text-muted-foreground",
                     item.disabled && "cursor-not-allowed opacity-80"
